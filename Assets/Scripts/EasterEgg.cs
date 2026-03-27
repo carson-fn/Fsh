15c921d507bde58d52807f8b9c1e1f7e86db3b02
Haroldmin1028 <harold.kim@ucalgary.ca>
2026-03-26 21:18:47 -0600

    Added Easter egg fishes, Instruction scene

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EasterEgg : MonoBehaviour, IPointerClickHandler
+{
+    [Header("Click Settings")]
+    public int clicksRequired = 5;
+
+    [Header("Easter Egg Objects")]
+    public GameObject millaine;
+    public GameObject carson;
+    public GameObject liam;
+    public GameObject sabrina;
+    public GameObject jacob;
+    public GameObject harold;
+
+    private int clickCount = 0;
+
+    void Start()
+    {
+        millaine.SetActive(false);
+        carson.SetActive(false);
+        liam.SetActive(false);
+        sabrina.SetActive(false);
+        jacob.SetActive(false);
+        harold.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        clickCount++;
+
+        if (clickCount >= clicksRequired)
+        {
+            millaine.SetActive(true);
+            carson.SetActive(true);
+            liam.SetActive(true);
+            sabrina.SetActive(true);
+            jacob.SetActive(true);
+            harold.SetActive(true);
+        }
+    }
+}
