383635033092747dbec8ab72de7fa1e715f0fe23
realvegetablegummy <144979434+realvegetablegummy@users.noreply.github.com>
2026-03-26 20:20:51 -0600

    NICE, logic works, moved to map script on map biome grid, arctic and beach not yet made but adding more should be easy, next, make level select ui better and connected, and make new sprites

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Map : MonoBehaviour
+{
+    // Ray Macros
+    private const int RAY_DISTANCE = 10; // distance to camera
+    private const int BIOME_LAYER = 1 << 8; // need bitmask from int
+
+    // Biome Names, TODO: make this into an enum?
+    private const string POND_NAME = "Pond";
+    private const string ARCTIC_NAME = "Lake"; // TODO: CHANGE THIS TO ARCTIC
+
+    // ------------------------------------------------------
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            TrySelectBiome();
+        }
+    }
+
+    private void TrySelectBiome()
+    {
+        Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition); // generate ray from mouse click
+        RaycastHit2D hit = Physics2D.GetRayIntersection(clickRay, RAY_DISTANCE, BIOME_LAYER);
+
+        if (hit.collider != null)
+        {
+            // mouseClick hit a biome
+            Debug.Log("mouse click hit a biome");
+
+            DifferentiateBiome(hit);
+        } else
+        {
+            // mouseClick hit nothing
+            Debug.Log("mouse click hit no biome");
+        }
+    }
+
+    private void DifferentiateBiome(RaycastHit2D hit)
+    {
+        string hitBiomeName = hit.collider.name;
+
+        switch (hitBiomeName)
+        {
+            case POND_NAME:
+                Debug.Log("mouse click hit pond biome, scene switching commence!");
+
+                SceneManager.LoadScene("PondLevelSelect");
+                Debug.Log("scene switched to PondLevelSelect");
+                break;
+
+            case ARCTIC_NAME:
+                Debug.Log("mouse click hit arctic biome, scene switching commence!");
+
+                SceneManager.LoadScene("ArcticLevelSelect");
+                Debug.Log("scene switched to ArcticLevelSelect");
+                break;
+
+            default:
+                Debug.Log("hit biome matches no game biome name");
+                break;
+        }
+    }
+}
