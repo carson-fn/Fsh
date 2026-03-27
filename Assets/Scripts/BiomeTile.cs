a109ebcf9d6cdddfd4df09e6e1a8d1117269cb41
realvegetablegummy <144979434+realvegetablegummy@users.noreply.github.com>
2026-03-26 19:53:01 -0600

    forgor to commit for a while, added biome sprites, added click raycast logic, need to fix so not null but specific to biome, try to add map script that calls specific biome scripts/scenes

--- Assets/Scripts/BiomeTile.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-[CreateAssetMenu(menuName = "Tiles/Biome Tile")]
-public class BiomeTile : Tile
-{
-    public string biomeName;
-}
---
+++ Assets/Scripts/Map/Beach.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Beach : MonoBehaviour
+{
+    private const int RAY_DISTANCE = 10; // distance to camera
+    private const int BIOME_LAYER = 1 << 8; // need bitmask from int
+
+    // ------------------------------------------------------
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            TrySelectBiome();
+        }
+    }
+
+    private void TrySelectBiome()
+    {
+        Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition); // generate ray from mouse click
+        RaycastHit2D hit = Physics2D.GetRayIntersection(clickRay, RAY_DISTANCE, BIOME_LAYER);
+
+        if (hit.collider != null)
+        {
+            // mouseClick hit biome
+            Debug.Log("mouse click hit beach biome, scene switching commence!");
+
+            SceneManager.LoadScene("BeachLevelSelect");
+            Debug.Log("scene switched to BeachLevelSelect");
+        } else
+        {
+            // mouseClick hit nothing
+            Debug.Log("mouse click hit no biome");
+        }
+    }
+}
